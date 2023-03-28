c00334639451faed3beaa893c956598bd2409cb9
HanaL123 <62302950+HanaL123@users.noreply.github.com>
2023-03-28 09:19:20 +0200

    Delete 1. SKLOP directory

--- 1. SKLOP/3. vaje/tretja_naloga.py
+++
@@ -1,6 +0,0 @@
-from prva_naloga import optimalni_tovor
-def optimalni_tovor_zaloga(predmeti, W):
-    nova = [(el[0], el[1]) for el in predmeti for _ in range(el[2])]
-    return optimalni_tovor(nova, W)
-
-test1 = print(optimalni_tovor_zaloga([(2,3, 1), (4,4, 2), (5,4, 4), (3,2, 3), (1,2, 3), (15, 12, 2)], 7))
